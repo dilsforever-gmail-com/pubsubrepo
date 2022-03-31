ca8fee765f04c51f7d2f8a3541e33e3af6f85595
Dileep <dileep.sasidharan@macys.com>
2022-03-31 07:15:07 -0400

    change topic

--- src/main/java/com/gcp/bigquerydemo/PubsubController.java
+++ src/main/java/com/gcp/bigquerydemo/PubsubController.java
@@ -1,5 +1,6 @@
 package com.gcp.bigquerydemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +9,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
+@Slf4j
 public class PubsubController {
+
 
     @Autowired
     private BigquerydemoApplication.PubsubOutboundGateway messagingGateway;
 
+    @PostMapping("/")
+    public ResponseEntity publishTest() {
+        Logger.getLogger("pubsub").info("In publish test success!!");
+        return new ResponseEntity<String>("Posted successfully",HttpStatus.OK);
+    }
+
     @PostMapping("/postMessage")
     public ResponseEntity publishMessage(@RequestParam("message") String message) {
+        Logger.getLogger("pubsub").info("In post message");
       messagingGateway.sendToPubsub(message);
         return new ResponseEntity<String>("Posted successfully",HttpStatus.CREATED);
     }
